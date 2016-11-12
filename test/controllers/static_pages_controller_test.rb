d79709febdc5c00db4bfb98a96888bba664c94a1
A5215 <a5215@users.noreply.github.com>
2016-11-12 15:13:56 +0300

    Make first pages and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+
+  test "should get root path" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get root url" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get log" do
+    get log_path
     assert_response :success
   end
 
